3d764b0ede10625e9f259a4fa9bcbb9666040298
JlblCblU4EPT <sashabarsasha228@yandex.ru>
2022-06-04 13:22:07 +0800

    Make appearance

---
+++ VkApp/ViewControllers/MainWindowViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainWindowViewController.swift
+//  VkApp
+//
+//  Created by Александр on 04.06.2022.
+//
+
+import UIKit
+
+class MainWindowViewController: UITabBarController {
+
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var questionButton: UIButton!
+    @IBOutlet weak var delButton: UIButton!
+    
+    
+    
+}
