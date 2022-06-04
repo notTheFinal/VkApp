cf2ac825c398f3b6dc64cf8d81735b720138545c
JlblCblU4EPT <sashabarsasha228@yandex.ru>
2022-06-04 12:37:07 +0800

    Initial Commit

---
+++ VkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VkApp
+//
+//  Created by Александр on 04.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
